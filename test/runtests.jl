410ec47e7aec24cb30b11eff5ae1cfd8a607cbca
David Smith <david.smith@gmail.com>
2017-11-18 23:37:49 -0600

    test fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,25 +5,24 @@ using Base.Test
 using Steganography
 
 @testset "steganography" begin
+    U = read("reliance.txt")
+    n = length(U)
     @testset "$S in $T" for S in [UInt8], T in [Float32, Float64]
-        n = 1024
         A = rand(T, n)
-        U = rand(S, n)
-        U7 = U .>> 1
         for j in 1:n
             r = setlast8(A[j], U[j])
             s = setlastbits(A[j], U[j], UInt8(8))
-            u = setlast7(A[j], U7[j])
-            v = setlastbits(A[j], U7[j], UInt8(7))
+            u = setlast7(A[j], U[j])
+            v = setlastbits(A[j], U[j], UInt8(7))
             @test r == s
             @test u == v
             @test getlast8(u) == getlastbits(u, UInt8(8))
             @test getlast7(u) == getlastbits(u, UInt8(7))
-            @test U7[j] == getlast7(u)
+            @test U[j] == getlast7(u)
             @test U[j] == getlast8(r)
         end
         A = rand(T, n + 2)
-        V = extract(embed(A, U7))
+        V = extract(embed(A, U))
         @test U == V
     end
 end
