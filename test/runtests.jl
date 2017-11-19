d31c458fbd555146710a5c29c3b15ec07f8ca780
David Smith <david.smith@gmail.com>
2017-11-19 16:57:21 -0600

    remove using to make nightly happy

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,6 @@
 include("../src/Steganography.jl")
 using Base.Test
 
-using Steganography
-
 @testset "steganography" begin
     U = read("reliance.txt")
     #println(join(Char.(U)))
@@ -11,21 +9,21 @@ using Steganography
     @testset "$S in $T" for S in [UInt8], T in [Int32, Int64, UInt32, UInt64, Float32, Float64]
         A = rand(T, n)
         for j in 1:n
-            r = setlast8(A[j], U[j])
-            s = setlastbits(A[j], U[j], UInt8(8))
-            u = setlast7(A[j], U[j])
-            v = setlastbits(A[j], U[j], UInt8(7))
+            r = Steganography.setlast8(A[j], U[j])
+            s = Steganography.setlastbits(A[j], U[j], UInt8(8))
+            u = Steganography.setlast7(A[j], U[j])
+            v = Steganography.setlastbits(A[j], U[j], UInt8(7))
             @test r == s
             @test u == v
-            @test getlast8(u) == getlastbits(u, UInt8(8))
-            @test getlast7(u) == getlastbits(u, UInt8(7))
-            @test U[j] == getlast7(u)
-            @test U[j] == getlast8(r)
+            @test Steganography.getlast8(u) == Steganography.getlastbits(u, UInt8(8))
+            @test Steganography.getlast7(u) == Steganography.getlastbits(u, UInt8(7))
+            @test U[j] == Steganography.getlast7(u)
+            @test U[j] == Steganography.getlast8(r)
         end
         A = rand(T, n + 2)
-        B = embed(A, U)
+        B = Steganography.embed(A, U)
         #println(B)
-        V = extract(B)
+        V = Steganography.extract(B)
         #println(join(Char.(V)))
         @test U == V
     end
